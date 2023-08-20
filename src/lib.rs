a18ba9cf50b6d84fb89cb8b1496fe3c999e95455
deko95 <kostya.shirolapov@gmail.com>
2023-08-20 02:16:37 -0400

    small setup

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,30 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::{fs, path::Path};
+
+pub struct Besida {
+    pub dialogue_nodes: Vec<DialogueNode>,
+}
+
+#[derive(Debug)]
+pub struct DialogueNode {
+    name: String,
+    speech: String,
+}
+
+fn parse(file_path: &Path) -> Vec<DialogueNode> {
+    let contents = fs::read_to_string(file_path).expect("should've read the file");
+    println!("{contents}");
+    vec![DialogueNode {
+        name: "You".into(),
+        speech: "First line".into(),
+    }]
+}
+
+impl Besida {
+    pub fn new(dialogue_file_path: &Path) -> Self {
+        Self {
+            dialogue_nodes: parse(dialogue_file_path),
+        }
+    }
 }
 
 #[cfg(test)]
@@ -8,7 +33,8 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        let dialogue_path = Path::new("examples/basic.bsd");
+        let besida = Besida::new(dialogue_path);
+        println!("{:?}", besida.dialogue_nodes);
     }
 }
